39692a746e134f4d3d401ae4001f41c21438c8fe
姜秀燕 <xiuyan.jiang@rokyinfo.com>
2018-01-15 17:35:47 +0800

    xiuyan.jiang
    新建项目

---
+++ js/main.js
@@ -0,0 +1,43 @@
+$(function () {
+
+  'use strict';
+
+  var $distpicker = $('#distpicker');
+
+  $distpicker.distpicker({
+    province: '福建省',
+    city: '厦门市',
+    district: '思明区'
+  });
+
+  $('#reset').click(function () {
+    $distpicker.distpicker('reset');
+  });
+
+  $('#reset-deep').click(function () {
+    $distpicker.distpicker('reset', true);
+  });
+
+  $('#destroy').click(function () {
+    $distpicker.distpicker('destroy');
+  });
+
+  $('#distpicker1').distpicker();
+
+  $('#distpicker2').distpicker({
+    province: '---- 所在省 ----',
+    city: '---- 所在市 ----',
+    district: '---- 所在区 ----'
+  });
+
+
+
+  $('#distpicker4').distpicker({
+    placeholder: false
+  });
+
+  $('#distpicker5').distpicker({
+    autoSelect: false
+  });
+
+});
